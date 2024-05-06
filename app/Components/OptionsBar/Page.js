691740b48bfb468cd9ce05b8e2498ee03217ec56
Saurashya <Saurashya1@gmail.com>
2024-05-06 11:09:25 +0545

    Saurashya

--- app/Components/OptionsBar/Page.js
+++ app/Components/OptionsBar/Page.js
@@ -6,7 +6,7 @@ const Page = () => {
   return (
   <nav className='bg-gray-300 font-semibold'>
     <ul className='flex first-line:items-center gap-6 px-4'>
-        <li className='cursor-pointer hover:text-red-500'><Link href="/Components/Hero">Home</Link></li>
+        <li className='cursor-pointer hover:text-red-500'><Link href="/">Home</Link></li>
         <li className='cursor-pointer hover:text-red-500'><Link href="/Components/Events">Events</Link></li>
         <li className='cursor-pointer hover:text-red-500'><Link href="/Components/Birth-Certificate">Birth Certificate</Link></li>
         <li className='cursor-pointer hover:text-red-500'><Link href="/Components/Marriage-Certificate">Marriage Certificate</Link></li>
